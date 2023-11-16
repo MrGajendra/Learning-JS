d65d490e0087aab70713be16a3d301ba3a4f4cbf
Gajendra Kumar <123275191+MrGajendra@users.noreply.github.com>
2023-11-16 14:12:30 +0530

    Update

---
+++ Info/README.md
@@ -0,0 +1,2 @@
+# Learning-JS
+ In this Repo I documented my whole journey of learning Javascript from Scrach
---
+++ JS-Basics/index.js
@@ -0,0 +1,17 @@
+console.log(`Hello, Gajendra Kumar`);
+//This is how we print info in js but it is not a part of core js it is a window object 
+
+//In Js for declaring value we use 
+// const for constant
+// let for variable
+// var for variable (not recomended for Modern js )
+
+//Examples 
+const AccountID = 1234;
+let AccountEmail = "gajendra@apple.com";
+var AccountPassword = "1234";
+
+AccountCity = "Korba";
+
+// AccountID = 4321; (it is not allowed because it is a constant only variables value can be change )
+
