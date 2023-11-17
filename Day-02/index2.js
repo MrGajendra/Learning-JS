fa3e2b1a079831f88d609aef55e791ae74af5f50
Gajendra Kumar <123275191+MrGajendra@users.noreply.github.com>
2023-11-17 11:00:27 +0530

    New note
    Added new notes about JS

---
+++ Day-02/index2.js
@@ -0,0 +1,8 @@
+"use strict"; //In JS when we want to treat our code as new or modern js we use this once we write this then the whole code base will be treated as the new JS (modern js) 
+
+//it is not necessary to use because by default code will ve treated as modern js
+
+// note ==>  alert(3 + 3); 
+//this will not work because we are using nodejs and it is the feature of browser that comes with window object.
+
+// writing code make sure that the code readability is high as much as you can make it 
